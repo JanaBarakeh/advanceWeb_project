674af3067b6906ff9d22b207b758f3dc7de067e8
salah aldin tanbour <salah.aldin.tanbour@outlook.com>
2024-08-18 20:48:12 +0300

    Add relationships between models in

--- Backend/RMS/app/Models/Reservation.php
+++ Backend/RMS/app/Models/Reservation.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Reservation extends Model
 {
@@ -14,4 +15,12 @@ class Reservation extends Model
         "status",
         "start_time"
     ];
+
+    public function table() : BelongsTo {
+        return $this->belongsTo(Table::class);
+    }
+
+    public function user() : BelongsTo {
+        return $this->belongsTo(User::class);
+    }
 }
--- Backend/RMS/app/Models/Table.php
+++ Backend/RMS/app/Models/Table.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Table extends Model
 {
@@ -14,4 +15,8 @@ class Table extends Model
         "details",
         "isPrivate"
     ];
+
+    public function reservations() : HasMany {
+        return $this->hasMany(Reservation::class);
+    }
 }
