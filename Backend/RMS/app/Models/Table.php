76899f9350df626c56de114533e9025cbe9ed730
lemarafuad <lemarafuad7@gmail.com>
2024-08-20 20:58:39 +0300

    solve 3

--- Backend/RMS/app/Models/Table.php
+++ Backend/RMS/app/Models/Table.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+
+
 class Table extends Model
 {
     use HasFactory;
