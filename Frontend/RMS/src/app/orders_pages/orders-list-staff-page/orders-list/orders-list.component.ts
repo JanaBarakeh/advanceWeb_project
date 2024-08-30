0df95edb93601c2e8f1725a7bbffb256cb5fcc00
farahelhasan <farahelhasan3@gmail.com>
2024-08-31 00:08:58 +0300

    add getAllOrder service

--- Frontend/RMS/src/app/orders_pages/orders-list-staff-page/orders-list/orders-list.component.ts
+++ Frontend/RMS/src/app/orders_pages/orders-list-staff-page/orders-list/orders-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { GetAllOrderForStaffService } from 'src/app/order-service/get-all-order-for-staff.service';
 
 @Component({
   selector: 'app-orders-list',
   templateUrl: './orders-list.component.html',
   styleUrls: ['./orders-list.component.css']
 })
-export class OrdersListComponent {
+export class OrdersListComponent implements OnInit {
+  orders = [];
+  orderService = inject(GetAllOrderForStaffService);
 
+  ngOnInit(): void {
+      this.getData();
+  }
+
+  getData(){
+    this.orderService.getData().subscribe(orders => {
+      this.orders = orders
+      console.log(this.orders);
+    })
+  }
 }
