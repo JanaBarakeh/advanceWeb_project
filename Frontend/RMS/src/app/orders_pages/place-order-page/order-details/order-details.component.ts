2d2556374937ea2bc52763d197141921bc783ded
farahelhasan <farahelhasan3@gmail.com>
2024-09-03 18:32:57 +0300

    add comments

--- Frontend/RMS/src/app/orders_pages/place-order-page/order-details/order-details.component.ts
+++ Frontend/RMS/src/app/orders_pages/place-order-page/order-details/order-details.component.ts
@@ -10,9 +10,15 @@ export class OrderDetailsComponent {
   // @Input() userId :any
   // @Input() reservationId: any
   
-  // i should replace them to be dynamic (pass or input)
+  // i should replace them to be dynamic (pass or input) (from lemara and salah)
   userId =1;
   reservationId=1;
+  // userId: number | undefined;
+  // reservationId: number | undefined;
+  // constructor(private route: ActivatedRoute) {}
 
-
+  // ngOnInit() {
+  //   this.userId = Number(this.route.snapshot.paramMap.get('userId'));
+  //   this.reservationId = String(this.route.snapshot.paramMap.get('reservationId'));
+  // }
 }
